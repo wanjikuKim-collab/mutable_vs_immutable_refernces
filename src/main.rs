11cc427c6297cc10a09dc5a03a0b8b44b87fc29c
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-02-27 15:28:50 +0300

    can't borrow s as mutable since it's also borrowed as immutable

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-let s = String::from("hello"); 
-let r1 = &s;
-let r2 = &s;
+    let mut s = String::from("hello"); 
+    let r1 = &s;
+    let r2 = &s;
 
-let r3 = &mut s;
+    let r3: &mut String = &mut s;
 
 println!("r1:{} and r2:{} are immutable references", r1, r2);
+
 }
